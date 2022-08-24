1d2f3fb106c020b7434733d3dd9ee8ec17a2ad05
RomelClavel <romeletoty@gmail.com>
2022-08-24 19:57:26 +0200

    fix(all): got rid of warnings

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { useFonts } from 'expo-font';
+import { PortalProvider } from '@gorhom/portal';
+import { LogBox } from 'react-native';
 import { store } from './app/store';
 import GilroyLight from './assets/fonts/Gilroy-Light.otf';
 import GilroyExtraBold from './assets/fonts/Gilroy-ExtraBold.otf';
@@ -11,7 +13,9 @@ import GilroyExtraBold from './assets/fonts/Gilroy-ExtraBold.otf';
 import useCachedResources from './hooks/useCachedResources';
 // import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { PortalProvider } from '@gorhom/portal';
+
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); // Ignore all log notifications
 
 export default function App() {
 	const isLoadingComplete = useCachedResources();
